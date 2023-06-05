1ad11e35b25018290cf16a67120d843c866ac17b
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-04 23:41:19 -0500

    Create Element Function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+/* JSX - HTML */
 function Greeting() {
-  return <h2>My First Component</h2>;
+  return (
+    <div>
+      <h2>My First Component</h2>
+    </div>
+  );
 }
+
+// function Greeting() {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h2", {}, "My First Component")
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
