e1c36db8924c28c527809d2a5a7cd73f449e7159
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-06 23:25:37 -0500

    BookList

--- src/index.js
+++ src/index.js
@@ -1,36 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-/* JSX - HTML */
-function Greeting() {
+const BookList = () => {
   return (
-    <>
-      <h2>My First Component</h2>
-      <Heading />
-      <Websites />
-    </>
-  );
-}
-
-/* Implicit Return */
-const Heading = () => <h3>Websites</h3>;
-
-/* Explicit Return */
-const Websites = () => {
-  return (
-    <ul>
-      <li>
-        <a href="https://react.dev/" target="blank">
-          React
-        </a>
-      </li>
-      <li>
-        <a href="https://www.google.com/">Google</a>
-      </li>
-    </ul>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 };
 
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/I/51B7kuFwQFL._SX329_BO1,204,203,200_.jpg"
+      alt="Atomic Habits"
+    />
+  );
+};
+const Title = () => <h2>Atomic Habits</h2>;
+const Author = () => <h4>James Clear </h4>;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Greeting />);
+root.render(<BookList />);
