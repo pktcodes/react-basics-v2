78224751a744d68484e7568af1c8dc82b343b76a
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-10 13:07:01 -0500

    Props - Somewhat Dynamic Setup

--- src/index.js
+++ src/index.js
@@ -3,15 +3,31 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
-const img = "./images/book-1.jpg";
-const title = "Atomic Habits: Build Good Habits";
-const author = "James Clear";
+const firstBook = {
+  img: "https://m.media-amazon.com/images/I/51Azhu94JlL._SX348_BO1,204,203,200_.jpg",
+  title: "Interesting Facts For Curious Minds",
+  author: "Jordan Moore",
+};
+
+const secondBook = {
+  img: "./images/book-1.jpg",
+  title: "Atomic Habits: Build Good Habits",
+  author: "James Clear",
+};
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book img={img} title={title} author={author} />
-      <Book img={img} title={title} author={author} />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      />
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   );
 };
