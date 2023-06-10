34d9c04143b60eff67fc8e32153188da5cd749bb
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-09 23:51:29 -0500

    Props - Basic Setup

--- src/index.js
+++ src/index.js
@@ -3,26 +3,26 @@ import ReactDOM from "react-dom/client";
 
 import "./index.css";
 
+const img = "./images/book-1.jpg";
+const title = "Atomic Habits: Build Good Habits";
+const author = "James Clear";
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book img={img} title={title} author={author} />
+      <Book img={img} title={title} author={author} />
     </section>
   );
 };
 
-const author = "James Clear";
-
-const Book = () => {
-  const title = "Atomic Habits: Build Good Habits";
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img src="./images/book-1.jpg" alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
     </article>
   );
 };
