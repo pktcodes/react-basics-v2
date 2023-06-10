31bfde8a8dbb6e323202c39fdd1cfd4eebb5828c
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-09 23:34:42 -0500

    JSX - Javascript

--- src/index.js
+++ src/index.js
@@ -14,29 +14,17 @@ const BookList = () => {
   );
 };
 
+const author = "James Clear";
+
 const Book = () => {
+  const title = "Atomic Habits: Build Good Habits";
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src="./images/book-1.jpg" alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
-};
-
-const Image = () => {
-  return <img src="./images/book-1.jpg" alt="Atomic Habits" />;
-};
-const Title = () => <h2>Atomic Habits</h2>;
-
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: "red",
-    fontSize: "0.75rem",
-    marginTop: "0.5rem",
-  };
-
-  return <h4 style={inlineHeadingStyles}>James Clear</h4>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
