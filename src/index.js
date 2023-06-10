09cfea8f285e77adaa7c81775ae84c0d33772b76
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-10 13:47:35 -0500

    Props - Multiple Approaches

--- src/index.js
+++ src/index.js
@@ -32,13 +32,13 @@ const BookList = () => {
   );
 };
 
-const Book = (props) => {
-  console.log(props);
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props;
   return (
     <article className="book">
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
     </article>
   );
 };
