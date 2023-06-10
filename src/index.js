b45ec10eedb144335ce2c36832c7a4ff142122fc
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-09 22:25:45 -0500

    JSX - CSS (Inline)

--- src/index.js
+++ src/index.js
@@ -28,7 +28,16 @@ const Image = () => {
   return <img src="./images/book-1.jpg" alt="Atomic Habits" />;
 };
 const Title = () => <h2>Atomic Habits</h2>;
-const Author = () => <h4>James Clear </h4>;
+
+const Author = () => {
+  const inlineHeadingStyles = {
+    color: "red",
+    fontSize: "0.75rem",
+    marginTop: "0.5rem",
+  };
+
+  return <h4 style={inlineHeadingStyles}>James Clear</h4>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
