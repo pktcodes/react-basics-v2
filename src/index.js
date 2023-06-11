2d70270deca2e498d9e12fcf8e7ecf26c99fb3f3
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-10 20:22:43 -0500

    Key Prop

--- src/index.js
+++ src/index.js
@@ -8,11 +8,13 @@ const books = [
     img: "https://m.media-amazon.com/images/I/51Azhu94JlL._SX348_BO1,204,203,200_.jpg",
     title: "Interesting Facts For Curious Minds",
     author: "Jordan Moore",
+    id: 1,
   },
   {
     img: "./images/book-1.jpg",
     title: "Atomic Habits: Build Good Habits",
     author: "James Clear",
+    id: 2,
   },
 ];
 
@@ -20,8 +22,8 @@ const BookList = () => {
   return (
     <section className="booklist">
       {books.map((book) => {
-        const { img, title, author } = book;
-        return <Book img={img} title={title} author={author} />;
+        const { img, title, author, id } = book;
+        return <Book key={id} img={img} title={title} author={author} />;
       })}
     </section>
   );
