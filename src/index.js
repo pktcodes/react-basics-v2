547eb69f0963a0f143195aefc04e492ccb799b2d
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-11 21:21:41 -0500

    Complex Example

--- src/index.js
+++ src/index.js
@@ -19,31 +19,34 @@ const books = [
 ];
 
 const BookList = () => {
-  const someValue = "shakeAndBake";
-
-  const displayValue = () => {
-    console.log(someValue);
+  const getBook = (id) => {
+    const findBook = books.find((book) => book.id === id);
+    console.log(findBook);
   };
 
   return (
     <section className="booklist">
       {books.map((book) => {
-        return <Book {...book} key={book.id} displayValue={displayValue} />;
+        return <Book {...book} key={book.id} getBook={getBook} />;
       })}
     </section>
   );
 };
 
 const Book = (props) => {
-  const { img, title, author, displayValue } = props;
+  const { img, title, author, id, getBook } = props;
   // console.log(props);
+
+  const getSingleBook = () => {
+    getBook(id);
+  };
 
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      <button onClick={displayValue}>Display Title</button>
+      <button onClick={() => getBook(id)}>Display Book</button>
     </article>
   );
 };
