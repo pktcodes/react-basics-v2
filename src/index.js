81056ea99acc06c59945574b9425c14c85f23b82
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-11 23:23:08 -0500

    Title Challenge

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ import Book from "./Book";
 
 const BookList = () => {
   return (
-    <section className="booklist">
-      {books.map((book, index) => {
-        return <Book {...book} key={book.id} number={index} />;
-      })}
-    </section>
+    <>
+      <h1 className="title">amazon best sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
   );
 };
 
