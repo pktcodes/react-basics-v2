b5220ce35abdc64aff55c2a8a74d578742ccad10
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-06-11 19:56:06 -0500

    Components Feature

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ const books = [
 const BookList = () => {
   return (
     <section className="booklist">
-      <EventExamples />
       {books.map((book) => {
         return <Book {...book} key={book.id} />;
       })}
@@ -29,32 +28,19 @@ const BookList = () => {
   );
 };
 
-const EventExamples = () => {
-  return (
-    <section>
-      <form>
-        <h2>Typical Form</h2>
-        <input
-          type="text"
-          name="product"
-          style={{ margin: "1rem 0" }}
-          onChange={(event) => console.log(event.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      <button onClick={() => console.log("Button Clicked")}>Click Me</button>
-    </section>
-  );
-};
-
 const Book = (props) => {
   const { img, title, author } = props;
   // console.log(props);
+
+  const displayTitle = () => {
+    console.log(title);
+  };
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
+      <button onClick={displayTitle}>Display Title</button>
     </article>
   );
 };
